d79e6dac7264b8585a2b47cb2bdb019da021d768
default <evan.maddicks@stu.ocsb.ca>
2025-09-19 14:39:07 +0000

    now says my name instead of world

---
+++ main.ts
@@ -0,0 +1,11 @@
+/**
+ * @author Evan Maddicks
+ * @version 1.0.0
+ * @date 2025-09-19
+ * @fileoverview this is the tradtitional "Hello, World!" program
+ */
+
+// print out the greeting
+console.log("Hello, Evan Maddicks");
+
+console.log("\nDone.");
